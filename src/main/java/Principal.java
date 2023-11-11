23c7f52d040ad36d3fe26f291dc6a0ff0acec2eb
Pablo Yllanes <yllanes.pablo@gmail.com>
2023-11-11 19:59:08 +0100

    Creada clase Carton.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
-        
+
+        Carton carton = new Carton();
+        String prueba = carton.toString();
+        System.out.println(prueba);
     }
 }
